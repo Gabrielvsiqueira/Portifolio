f88489874dc9ed8b54d17e2cdd8c86a2740ea9b1
Gabe Siqueira <gabesiqueira25@gmail.com>
2025-05-25 21:03:41 -0300

    feat: refatorando meu componente button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,19 +2,23 @@ import type { ReactNode } from 'react';
 import { Button } from 'react-bootstrap';
 
 type MyButtonProps = {
-  label: string;
+  label?: string;
   onClick: () => void;
   type?: 'button' | 'submit' | 'reset';
-  variant?: 'primary' | 'secondary' | 'success' | 'warning' | 'danger' | 'info' | 'light' | 'Dark' | 'Link';
+  variant?: 'primary' | 'secondary' | 'success' | 'warning' | 'danger' | 'info' | 'light' | 'dark' | 'link';
   size?: 'sm' | 'lg';
   disabled?: boolean;
   icon?: ReactNode;
-}
+  className?: string; 
+};
 
-function MyButton({label, onClick, type= 'button', variant='light'}: MyButtonProps) {
+function MyButton({ label, onClick, type = 'button', variant = 'light', size, disabled = false, icon, className = '' }: MyButtonProps){
   return (
-    <Button variant={variant} type={type} onClick={onClick}>{label}</Button>
-  )
+    <Button variant={variant} type={type} className={`my-button-base ${className}`} onClick={onClick} size={size} disabled={disabled}>
+      {icon && <span style={{ marginRight: label ? '0.5rem' : 0 }}>{icon}</span>}
+      {label}
+    </Button>
+  );
 }
 
 export default MyButton;
