ff47960704b9403d38f45177534bd20201133d7c
Gabe Siqueira <gabesiqueira25@gmail.com>
2025-05-25 21:05:47 -0300

    feat: adicionando lib lucile de icons ao projeto e estilizando novos componentes

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,36 @@
 import "./style.css";
 import MyHeader from "./components/Header";
 import MyButton from "./components/Button";
+import { Github, Instagram, Linkedin, Youtube} from 'lucide-react';
 
 function App() {
   return (
     <>
     <MyHeader />
     <div className="about">
+
         <div className="text-content">
           <h1 className="tittle">HELLO & WELCOME!</h1>
           <h3 className="subtittle">
             I'm Gabriel Vitor Siqueira, a Front-End Developer passionate about UI design and turning ideas into code.
           </h3>
-          <div className="Informations">
-          <MyButton label="My Career"/>
-          <MyButton label="Rsumé"/>
+
+          <div className="informations">
+            <div className="buttons-information">
+              <MyButton label="My Career" className="my-button" onClick={() => window.open('https://www.linkedin.com/in/gabriel-vitor-siqueira/','_blank')}/>
+              <MyButton label="Resumé" className="my-button" onClick={() => window.open('../public/resume.pdf', '_blank')}/>
+            </div>
+
+            <div className="icon-information">
+              <MyButton icon={<Github size={20} />} className="my-button" onClick={() => window.open('https://github.com/Gabrielvsiqueira', '_blank')} />
+              <MyButton icon={<Linkedin size={20} />} className="my-button" onClick={() => window.open('https://www.linkedin.com/in/gabriel-vitor-siqueira/',     '_blank')} />
+              <MyButton icon={<Instagram size={20} />} className="my-button" onClick={() => window.open('https://www.instagram.com/siqueiragv/', '_blank')} />
+              <MyButton icon={<Youtube size={20} />} className="my-button" onClick={() => window.open('https://www.youtube.com/@siqueiragv', '_blank')} />
+            </div>
           </div>
+
         </div>
-        <img src="/perfil.jpeg" alt="A man hair and moustache black." />
+        <img src="../public/perfil.png" alt="A man hair and moustache black." />
       </div>
 
     </>
